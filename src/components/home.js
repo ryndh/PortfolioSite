d03dfd8d7dfdd14853889f9a76f06ecda6f42fa6
ryanhull <ryhu23@gmail.com>
2019-02-08 13:53:41 -0700

    overhaul

--- src/components/home.js
+++ src/components/home.js
@@ -35,22 +35,22 @@ export default class Home extends Component {
       tool.forEach((item) => {
         item.classList.add('bigger')
       })
-      lines.forEach((line)=> {
+      lines.forEach((line) => {
         line.classList.add('line')
       })
     } else {
       tool.forEach((item) => {
         item.classList.remove('bigger')
       })
-      lines.forEach((line)=> {
+      lines.forEach((line) => {
         line.classList.remove('line')
       })
     }
 
   }
-  
+
   click = (e) => {
-    switch(e.target.name) {
+    switch (e.target.name) {
       case 'home':
         window.scrollTo({
           top: 0,
@@ -71,37 +71,35 @@ export default class Home extends Component {
         break
     }
   }
-  
+
   render() {
-    let size = { backgroundSize: `${120 + (this.state.offset / 4)}%` }
-    let sizePhone = { backgroundSize: `${150 + (this.state.offset / 4)}%` }
+    let size = { backgroundSize: `${100 + (this.state.offset / 4)}%` }
+    let sizePhone = { backgroundSize: `${120 + (this.state.offset / 4)}%` }
     let width = window.innerWidth;
     return (
       <div className='app'>
         <div className='navbar'>
           <a onClick={this.click} name='home' className='link' >Home</a>
-          <a onClick={this.click} name='proj'className='link' >Projects</a>
-          <a onClick={this.click} name='contact'className='link' >Contact</a>
+          <a onClick={this.click} name='proj' className='link' >Projects</a>
+          <a onClick={this.click} name='contact' className='link' >Contact</a>
         </div>
         <div className='main-body'>
           <div className='main-body-content'>
-            <div className='top-grid' style={width < 700 ? sizePhone : null}>
-              <div className='top-left'>
-                <p className='ryan'>Hey there, I'm <span> Ryan</span></p>
-
-              </div>
-              <div className='top-right' style={size}></div>
+            <div className='top' style={width < 700 ? sizePhone : null} style={size}>
+                <p className='ryan'><span>Ryan Hull</span></p>
+                <p className='dev'><span>Full Stack Developer</span></p>
             </div>
             <div className='projects-wrapper' id='proj'>
               <h1 className='projects-heading'>Projects</h1>
               <div className='projects-grid'>
                 {this.state.projects.map((project, index) => {
                   return (
-                    <a className='project' href={project.url} target='_blank' key={index}>
-                      <h1>{project.title}</h1>
-                      <h3 href={project.url} target='_blank'>Check it out!</h3>
+                    <a className={`project ${project.classname}`} href={project.url} target='_blank' key={index}>
+
                       <div className='description'>
+                        <h1>{project.title}</h1>
                         <p>{project.description}</p>
+                        <h3 href={project.url} target='_blank'>Check it out!</h3>
                       </div>
                     </a>
                   )
@@ -115,7 +113,7 @@ export default class Home extends Component {
                   return (
                     <div className='tool' key={index}>
                       <h1 className='h-expand'>{tool.title}</h1>
-                      <i className={`${tool.icon} scale-tool`}></i>
+                      <img className='scale-tool' src={tool.icon}></img>
                     </div>
                   )
                 })}
@@ -125,13 +123,14 @@ export default class Home extends Component {
               <h1 className='contact-heading'>Get In Touch</h1>
               <div className='contact-grid'>
                 <div className='contact'>
-                  <h3>I'm always open to new ideas or opportunities! <br/> Send me a message on LinkedIn and let's talk!</h3>
-                  <a href='https://www.linkedin.com/in/ryan-hull-3b4806b9/' target='_blank' className='linkedin-link'><i className="fab fa-linkedin"></i></a>
+                  <h3>I'm always open to new ideas or opportunities! <br /> Send me a message on LinkedIn and let's talk!</h3>
+                  <a href='https://www.linkedin.com/in/ryan-hull-3b4806b9/' target='_blank' className='linkedin-link'>
+                  <img className="linkedin" src={'../../static/assets/linkedin.png'}></img></a>
                 </div>
               </div>
             </div>
             <div className='footer-wrapper'>
-                <div className='footer'>
+              <div className='footer'>
                 &copy; 2018  &#124; Ryan
                 </div>
             </div>
--- src/components/projectsTools.js
+++ src/components/projectsTools.js
@@ -3,50 +3,56 @@ export const projectsTools = {
       {
         'title': 'Movie Micro Service',
         'url': 'https://moviereactdb.herokuapp.com/',
-        'description': 'React/Python project with a PostgreSQL database'
+        'description': 'React/Python project with a PostgreSQL database',
+        'classname': 'project-movie'
       },
 
       {
         'title': 'Diner',
         'url': 'https://reactdiner.herokuapp.com',
-        'description': "Diner with shopping cart and checkout functionality"
+        'description': "Diner with shopping cart and checkout functionality",
+        'classname': 'project-diner'
       },
       {
         'title': 'Mad Libs',
         'url': 'https://reactmadlibs.herokuapp.com/',
-        'description': 'React app with multiple madlibs that can be cycled through'
+        'description': 'React app with multiple madlibs that can be cycled through',
+        'classname': 'project-mad'
+
       },
 
       {
         'title': 'Calculator',
         'url': 'https://basic-react-calculator.herokuapp.com/',
-        'description': 'Calculator with basic functionality, gradient that changes based on value'
+        'description': 'Calculator with basic functionality, gradient that changes based on value',
+        'classname': 'project-calc'
+
       },
     ],
     tools: [
       {
         'title': 'React',
-        'icon': 'fab fa-react',
+        'icon': "../../static/assets/react.png",
       },
       {
         'title': 'Python',
-        'icon': 'fab fa-python',
+        'icon': "../../static/assets/python.png",
       },
       {
         'title': 'SCSS/CSS',
-        'icon': 'fab fa-css3-alt',
+        'icon': "../../static/assets/sass.png",
       },
       {
         'title': 'HTML5',
-        'icon': 'fab fa-html5',
+        'icon': "../../static/assets/html.png",
       },
       {
         'title': 'Git',
-        'icon': 'fab fa-git',
+        'icon': "../../static/assets/git.png",
       },
       {
         'title': 'Javascript',
-        'icon': 'fab fa-js-square',
+        'icon': "../../static/assets/js.png",
       },
     ],
     offset: 0,
