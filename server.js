fb98d0cde485d014facc454276b6cde4d38a7c37
Ryan Hull <ryan.hull@familysearch.org>
2019-07-10 08:17:30 -0600

    adding express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = process.env.PORT || 8000;
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, '/dist/')));
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+app.listen(port);
+
+console.log("server started");
